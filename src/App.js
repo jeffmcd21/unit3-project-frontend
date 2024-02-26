c9757c298ed876d765cef24c19955387a0606969
Jeff McDonald <jwmcdonald85@gmail.com>
2024-02-25 20:19:24 -0500

    Create Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return(
     <div className="App">
-      <Outlet />
       <Header />
+      <Outlet />
+      <Footer />
       </div>
   )
 }
