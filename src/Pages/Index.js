9581be13bf2190d2eac3cbb91c80e1d891755564
Jeff McDonald <jwmcdonald85@gmail.com>
2024-02-27 15:57:38 -0500

    create search option

--- src/Pages/Index.js
+++ src/Pages/Index.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { useLoaderData, Link, Form} from "react-router-dom";
+import { useState } from "react";
 
 const Landing = () => {
   const events = useLoaderData();
 
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = (action) => {
+    setSearchQuery(action.target.value)
+  };
+
   return (
     <div>
+      <input type="text" placeholder="Search bookmarks..." className="search-bar" onChange={handleSearch}/>
       <h3>Create an Event</h3>
       <Form action='/create' method='post'>
         <input type="input" name="name" placeholder="Event name" />
@@ -17,7 +25,8 @@ const Landing = () => {
       </Form>
 
       <h3>Events</h3>
-      {events.map((event) => (
+      {events.filter((event) => 
+      event.name.toLowerCase().includes(searchQuery.toLowerCase())).map((event) => (
         <div key={event._id} className="events">
           <Link to={`/${event._id}`}>
             <h1>{event.name}</h1>
