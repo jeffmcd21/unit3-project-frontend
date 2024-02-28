f12b1cbdada5682b51ec2d6ddfb8bc6c46d1d2dd
Jeff McDonald <jwmcdonald85@gmail.com>
2024-02-27 22:46:07 -0500

    styles update

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom"
 
 const Header = () => {
     return(
+      <div className="background-image">
         <nav className="nav">
         <Link to="/">
           <div>Event app</div>
         </Link>
       </nav>
+    </div>
     )
 }
 export default Header
